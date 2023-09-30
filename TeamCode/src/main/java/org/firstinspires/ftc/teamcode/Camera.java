7f344b78ca254e93497995140ea8bb80cc891a0e
Sam <sam.maddox1@yahoo.com>
2023-09-30 15:49:49 -0400

    Project now successfully triggers some variants of the unsafe crash bug. I haven't observed some other situations I suspect are related, so they may not be tied to the same problem.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Camera.java
@@ -0,0 +1,59 @@
+package org.firstinspires.ftc.teamcode;
+
+import android.util.Size;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagGameDatabase;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+public class Camera {
+
+    public AprilTagProcessor aprilTag;
+
+    public RedProp redProp;
+    public VisionPortal visionPortal;
+
+
+
+
+    public Camera(HardwareMap hardwareMap)
+    {
+        // Create the AprilTag processor.
+        aprilTag = new AprilTagProcessor.Builder()
+                .setDrawAxes(false)
+                .setDrawCubeProjection(false)
+                .setDrawTagOutline(true)
+                .setTagFamily(AprilTagProcessor.TagFamily.TAG_36h11)
+                .setTagLibrary(AprilTagGameDatabase.getCenterStageTagLibrary())
+                .setOutputUnits(DistanceUnit.INCH, AngleUnit.DEGREES)
+
+                // == CAMERA CALIBRATION ==
+                // If you do not manually specify calibration parameters, the SDK will attempt
+                // to load a predefined calibration for your camera.
+                //.setLensIntrinsics(578.272, 578.272, 402.145, 221.506)
+        // ... these parameters are fx, fy, cx, cy.
+                .build();
+            redProp = new RedProp();
+
+        //Build The Vision Portal
+        visionPortal = new VisionPortal.Builder()
+                .setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"))
+                .enableLiveView(true)
+                .setAutoStopLiveView(false)
+                .setCameraResolution(new Size(640, 480))
+                .addProcessors(aprilTag ,redProp)//, coneFinder, tensorflow)
+                .build();
+
+
+
+
+
+
+    }
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorProcessor.java
@@ -0,0 +1,333 @@
+package org.firstinspires.ftc.teamcode;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+
+import org.firstinspires.ftc.robotcore.internal.camera.calibration.CameraCalibration;
+import org.firstinspires.ftc.vision.VisionProcessor;
+import org.opencv.android.Utils;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class ColorProcessor implements VisionProcessor {
+
+    // Does the vision processor see anything?
+    private boolean hasResult = false;
+
+    // Intermediate images saved by the pipeline
+    // They are instance variable up here to avoid creating and throwing away
+    // new pictures every time
+    public Mat blurred = new Mat();
+    public Mat hsv = new Mat();
+    public Mat thresholded = new Mat();
+
+
+    public Mat th = new Mat();
+    public Mat ts = new Mat();
+    public Mat tv = new Mat();
+
+    // Contours detected in the thresholded mat
+    public List<MatOfPoint> contours = new ArrayList<>();
+
+    // Bounding box around the biggest detected object
+    private Rect boundingBox = new Rect();
+
+    // Center of the bounding box
+    private Point center = new Point();
+
+
+    // For drawing over top of the Android canvas
+    //public Mat overlay;
+
+    public int w;
+    public int h;
+
+    public boolean crashNow = false;
+
+
+    /**
+     * Set up the pipeline.
+     * @param width Width of incoming images
+     * @param height Height of incoming images
+     * @param calibration Camera calibration data
+     */
+    @Override
+    public void init(int width, int height, CameraCalibration calibration) {
+        w = width;
+        h = height;
+
+        //overlay = new Mat(h, w, CvType.CV_8UC4);
+        //Bitmap camOut = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
+    }
+
+    /**
+     * Process a single picture from the camera.
+     * @param frame The incoming picture
+     * @param captureTimeNanos Time in nanoseconds it took to take the picture
+     * @return Data to be passed to onDrawFrame
+     */
+    @Override
+    public Object processFrame(Mat frame, long captureTimeNanos) {
+
+        // TODO Fill in the image processing steps from the Imgproc libarary
+
+        // Blur the image slightly
+        // Params: (frame, blurred, new Size(11,11), 1.0)
+            Imgproc.GaussianBlur(frame, blurred, new Size(11,11), 1.0);
+
+        // To HSV
+        // Params: (blurred, hsv, Imgproc.COLOR_RGB2HSV)
+            Imgproc.cvtColor(blurred, hsv, Imgproc.COLOR_RGB2HSV);
+        // Threshold the image
+        // Note: This one is in Core, not Imgproc
+        // Params: (hsv, minColor(), maxColor(), thresholded)
+            Core.inRange(hsv, minColor(), maxColor(), thresholded);
+
+        contours.clear();
+        // Find contours (blobs) in the image
+        // Params: (thresholded, contours, new Mat(), Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_SIMPLE)
+        Imgproc.findContours(thresholded, contours, new Mat(), Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_SIMPLE);
+
+        /*
+        TODO
+        Now we need to pick the biggest "contour" on the image.
+        A "contour" is just a fancy word for a blob of color, or the outlines around it.
+        Using a loop, go through the `contours` variable and set `biggestIndex` to the
+        index of the largest one.
+        Also, make sure to ignore contours less than `minArea()` in size!
+        HINT 1: You will need a loop!
+        HINT 2: contours.get(7) gets contour index 7
+        HINT 3: Imgproc.contourArea(c) calculates the area of contour c
+         */
+        // Pick the biggest contour
+        int biggestIndex = -1;
+        double biggestArea = 0.0;
+        for(int j=0; j<contours.size(); j++)
+        {
+            double a = Imgproc.contourArea(contours.get(j));
+            if(biggestArea < a  && a > minArea()){
+                biggestArea = a ;
+                biggestIndex = j;}
+        }
+
+
+
+        // Did we find anything?
+        if(biggestIndex >= 0)
+        {
+            hasResult = true;
+
+            boundingBox = Imgproc.boundingRect(contours.get(biggestIndex));
+            center = new Point(
+                    (boundingBox.br().x + boundingBox.tl().x)/2,
+                    (boundingBox.br().y + boundingBox.tl().y)/2);
+        }
+        else
+        {
+            hasResult = false;
+        }
+
+        // By converting to an array, this creates a new object that won't
+        // cause multithreading insanity when accessed by onDrawFrame
+        return contours.toArray();
+    }
+
+    /**
+     * Draw the results of the vision processor on the robot's "screen".
+     * @param canvas Canvas for drawing on
+     * @param onscreenWidth Actual screen width
+     * @param onscreenHeight Actual screen height
+     * @param scaleBmpPxToCanvasPx Actual screen size / input size
+     * @param scaleCanvasDensity Undocumented
+     * @param userContext Object passed from processedFrame
+     */
+    @Override
+    public void onDrawFrame(Canvas canvas, int onscreenWidth, int onscreenHeight, float scaleBmpPxToCanvasPx, float scaleCanvasDensity, Object userContext) {
+
+        // Crash!
+        if(crashNow) {
+            int x = 100 / 0;
+        }
+
+
+        // Set up paint
+        Paint p = new Paint();
+        p.setColor(Color.BLUE);
+        p.setStrokeWidth(5);
+        p.setStyle(Paint.Style.STROKE);
+
+        // Draw bounding box
+        if(hasResult) {
+            canvas.drawRect(boundingBox.x * scaleBmpPxToCanvasPx, boundingBox.y * scaleBmpPxToCanvasPx,
+                    (boundingBox.width + boundingBox.x) * scaleBmpPxToCanvasPx, (boundingBox.height + boundingBox.y) * scaleBmpPxToCanvasPx, p);
+        }
+
+        // Manually draw contour points
+        p.setColor(Color.YELLOW);
+        p.setStrokeWidth(3);
+        for (Object o : (Object[]) userContext) {
+            MatOfPoint contour = (MatOfPoint) o;
+            Point[] pts = contour.toArray();
+
+            if(pts.length > 1) {
+                for (int j = 0; j < pts.length - 1; j++) {
+                    //canvas.drawPoint((float) pts[j].x * scaleBmpPxToCanvasPx, (float) pts[j].y * scaleBmpPxToCanvasPx, p);
+                    canvas.drawLine((float) pts[j].x * scaleBmpPxToCanvasPx, (float) pts[j].y * scaleBmpPxToCanvasPx,
+                            (float) pts[j+1].x * scaleBmpPxToCanvasPx, (float) pts[j+1].y * scaleBmpPxToCanvasPx,
+                            p);
+                }
+                canvas.drawLine((float) pts[0].x * scaleBmpPxToCanvasPx, (float) pts[0].y * scaleBmpPxToCanvasPx,
+                        (float) pts[pts.length - 1].x * scaleBmpPxToCanvasPx, (float) pts[pts.length - 1].y * scaleBmpPxToCanvasPx,
+                        p);
+            }
+        }
+
+        // Draw threshold in corner
+        Bitmap camOut = Bitmap.createBitmap(w, h, Bitmap.Config.RGB_565);
+        Utils.matToBitmap(thresholded, camOut);
+        canvas.drawBitmap(camOut, null, new android.graphics.Rect((int) (onscreenWidth * 0.75),
+                (int) (onscreenHeight * 0.75),
+                (int) (onscreenWidth),
+                (int) (onscreenHeight)), null);
+
+        // Draw HSV in corner
+        camOut = Bitmap.createBitmap(w, h, Bitmap.Config.RGB_565);
+        Utils.matToBitmap(hsv, camOut);
+        canvas.drawBitmap(camOut, null, new android.graphics.Rect((int) (onscreenWidth * 0.75),
+                (int) (onscreenHeight * 0.5),
+                (int) (onscreenWidth),
+                (int) (onscreenHeight * 0.75)), null);
+
+
+        // Draw individual H,S, and V thresholds
+
+        Core.inRange(hsv, new Scalar(minColor().val[0],0,0), new Scalar(maxColor().val[0],255,255), th);
+        Core.inRange(hsv, new Scalar(0,minColor().val[1],0), new Scalar(255,maxColor().val[1],255), ts);
+        Core.inRange(hsv, new Scalar(0,0,minColor().val[2]), new Scalar(255,255,maxColor().val[2]), tv);
+
+        camOut = Bitmap.createBitmap(w, h, Bitmap.Config.RGB_565);
+        Utils.matToBitmap(th, camOut);
+        canvas.drawBitmap(camOut, null, new android.graphics.Rect((int) (0),
+                (int) (0),
+                (int) (onscreenWidth * 0.20),
+                (int) (onscreenHeight * 0.15)), null);
+
+        camOut = Bitmap.createBitmap(w, h, Bitmap.Config.RGB_565);
+        Utils.matToBitmap(ts, camOut);
+        canvas.drawBitmap(camOut, null, new android.graphics.Rect((int) (onscreenWidth * 0.20),
+                (int) (0),
+                (int) (onscreenWidth * 0.40),
+                (int) (onscreenHeight * 0.15)), null);
+
+        camOut = Bitmap.createBitmap(w, h, Bitmap.Config.RGB_565);
+        Utils.matToBitmap(tv, camOut);
+        canvas.drawBitmap(camOut, null, new android.graphics.Rect((int) (onscreenWidth * 0.40),
+                (int) (0),
+                (int) (onscreenWidth * 0.60),
+                (int) (onscreenHeight * 0.15)), null);
+
+
+        // Draw color blocks for min and max
+        p.setStyle(Paint.Style.FILL);
+        p.setColor(Color.HSVToColor(new float[]{(float) (minColor().val[0] * 360.0/255.0), (float) (minColor().val[1] / 255.0),(float) (minColor().val[2] / 255.0)}));
+        canvas.drawRect(new android.graphics.Rect((int) (onscreenWidth * 0.45),
+                (int) (onscreenHeight * 0.95),
+                (int) (onscreenWidth * 0.5),
+                (int) (onscreenHeight)), p);
+
+        p.setColor(Color.HSVToColor(new float[]{(float) (maxColor().val[0] * 360.0/255.0), (float) (maxColor().val[1] / 255.0),(float)  (maxColor().val[2] / 255)}));
+        canvas.drawRect(new android.graphics.Rect((int) (onscreenWidth * 0.5),
+                (int) (onscreenHeight * 0.95),
+                (int) (onscreenWidth * 0.55),
+                (int) (onscreenHeight)), p);
+
+        // Draw contours overlay
+        /*
+        overlay.setTo(new Scalar(0,0,0,255));
+        Imgproc.drawContours(overlay, contours, -1, new Scalar(255,255,0),5);
+
+        camOut = Bitmap.createBitmap(w, h, Bitmap.Config.RGB_565);
+        Utils.matToBitmap(overlay, camOut);
+
+        canvas.drawBitmap(camOut, null, new android.graphics.Rect((int) 0,
+                (int) 0,
+                (int) (onscreenWidth),
+                (int) (onscreenHeight)), null); */
+
+
+
+        /*p.setColor(Color.YELLOW);
+        for (MatOfPoint contour : (List<MatOfPoint>) userContext) {
+            Point[] pts = contour.toArray();
+
+
+            Arrays.stream(pts).map(pt -> {return new float[(float) pt.x * scaleBmpPxToCanvasPx, (float) pt.y * scaleBmpPxToCanvasPx];});
+            canvas.drawPoints();
+        }*/
+    }
+
+    /**
+     * Does this color processor see anything?
+     * @return True if this processor sees something, false otherwise
+     */
+    public boolean hasResult()
+    {
+        return hasResult;
+    }
+
+    /**
+     * Get the bounding box surrounding the biggest thing we see.
+     * @return a bounding box around the biggest object seen
+     */
+    public Rect getBoundingBox()
+    {
+        return boundingBox;
+    }
+
+
+    /**
+     * Get a point representing the center of the largest object seen.
+     * This is the center of the bounding box, not the center of mass.
+     * @return A point representing the center of the object.
+     */
+    public Point getCenter()
+    {
+        return center;
+    }
+
+
+
+
+    /**
+     * Get a scalar containing the maximum Hue, Saturation, and Value of pixels that match
+     * our target color.
+     * @return a Scalar containing the maximum desired Hue, Saturation, and Value.
+     */
+    public abstract Scalar maxColor();
+
+    /**
+     * Get a scalar containing the minimum Hue, Saturation, and Value of pixels that match
+     * our target color.
+     * @return a Scalar containing the minimum desired Hue, Saturation, and Value.
+     */
+    public abstract Scalar minColor();
+
+    /**
+     * Get the minimum area that the object we are looking for should take up on the camera.
+     * @return The least area our target should take up. Anything smaller than this will be ignored.
+     */
+    public abstract double minArea();
+
+
+}
